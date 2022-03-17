fc84d1b204dd3db0c447c4b7aefd5482e7919c70
Raphael Berger <raphaelmb@gmail.com>
2022-03-17 15:21:22 -0300

    Updates

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,26 @@
 import { Service } from "./service.js";
+import { logger } from "./util.js";
 
 export class Controller {
   constructor() {
     this.service = new Service();
   }
+
   async getFileStream(fileName) {
     return this.service.getFileStream(fileName);
+  }
+
+  createClientStream() {
+    const { id, clientStream } = this.service.createClientStream();
+
+    const onClose = () => {
+      logger.info(`Closing connection of ${id}`);
+      this.service.removeClientStream(id);
+    };
+
+    return {
+      stream: clientStream,
+      onClose,
+    };
   }
 }
--- server/service.js
+++ server/service.js
@@ -3,18 +3,29 @@ import fsPromises from "fs/promises";
 import { randomUUID } from "crypto";
 import config from "./config.js";
 import { join, extname } from "path";
-import { PassThrough } from "stream";
+import { PassThrough, Writable } from "stream";
+import Throttle from "throttle";
+import childProcess from "child_process";
+import { logger } from "./util.js";
+import streamsPromises from "stream/promises";
+import { once } from "events";
 
 const {
   dir: { publicDirectory },
+  constants: { fallbackBitRate, englishConversation, bitRateDivisor },
 } = config;
 
 export class Service {
   constructor() {
     this.clientStreams = new Map();
+    this.currentSong = englishConversation;
+    this.currentBitRate = 0;
+    this.throttleTransform = {};
+    this.currentReadable = {};
+    this.startStream();
   }
 
-  getClientStream() {
+  createClientStream() {
     const id = randomUUID();
     const clientStream = new PassThrough();
     this.clientStreams.set(id, clientStream);
@@ -27,6 +38,55 @@ export class Service {
 
   removeClientStream(id) {
     this.clientStreams.delete(id);
+  }
+
+  _executeSoxCommand(args) {
+    childProcess.spawn("sox", args);
+  }
+
+  async getBitRate(song) {
+    try {
+      const args = ["--i", "-B", song];
+      const { stderr, stdout, stin } = this._executeSoxCommand(args);
+      console.log(stdout);
+
+      await Promise.all([once(stderr, "readable"), once(stdout, "readable")]);
+
+      const [success, error] = [stdout, stderr].map((stream) => stream.read());
+      if (error) return await Promise.reject(error);
+
+      return success.toString().trim().replace(/k/, "000");
+    } catch (error) {
+      logger.error(`Error on bitrate: ${error}`);
+      return fallbackBitRate;
+    }
+  }
+
+  broadCast() {
+    return new Writable({
+      write: (chunk, enc, cb) => {
+        for (const [id, stream] of this.clientStreams) {
+          // if client disconnected should not send more data
+          if (stream.writableEnded) {
+            this.clientStreams.delete(id);
+            continue;
+          }
+          stream.write(chunk);
+        }
+        cb();
+      },
+    });
+  }
+
+  async startStream() {
+    logger.info(`Starting with ${this.currentSong}`);
+    const bitRate = (this.currentBitRate =
+      (await this.getBitRate(this.currentSong)) / bitRateDivisor);
+    const throttleTransform = new Throttle(bitRate);
+    const songReadable = (this.currentReadable = this.createFileStream(
+      this.currentSong
+    ));
+    streamsPromises.pipeline(songReadable, throttleTransform, this.broadCast());
   }
 
   createFileStream(filename) {
